bed078ec802c718a609d69a5a6f5eab40dd98e12
Gabe-Bryan <gabriel.bryan544@gmail.com>
2022-05-06 16:52:34 -0700

    iteration 1 setup for others

--- src/module-info.java
+++ src/module-info.java
@@ -3,12 +3,12 @@ module MacroSoft.TCSS360 {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+//    requires org.controlsfx.controls;
+//    requires com.dlsc.formsfx;
+//    requires validatorfx;
+//    requires org.kordamp.ikonli.javafx;
+//    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
 
     opens main to javafx.fxml;
     exports main;
