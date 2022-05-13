2c73fb069ec5f45ed11719b3168bad8a7b3fa7b9
paullee100 <leepaul863@gmail.com>
2022-05-12 17:48:14 -0700

    Updated GUI with login screen

--- src/main/GUIApplication.java
+++ src/main/GUIApplication.java
@@ -1,16 +1,12 @@
 package main;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class GUIApplication extends Application {
     private final Version myVersion = new Version();
@@ -20,25 +16,86 @@ public class GUIApplication extends Application {
     public void start(Stage stage) {
         myUser = new User("ExampleBob", "ExampleBob@gmail.com");
 
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(100, 100, 100, 100));
-
-        gridPane.setVgap(5);
-        gridPane.setHgap(5);
-
-        gridPane.setAlignment(Pos.CENTER);
-        versionBox(gridPane);
-
-        Text text1 = new Text("Welcome...");
-        gridPane.add(text1, 1, 1);
-
-        Scene scene = new Scene(gridPane);
+        Scene scene = new Scene(loginScreen(stage), 500, 500);
         stage.setTitle("IterationOne");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
-    public void versionBox(GridPane gridPane) {
+    private BorderPane mainScreen() {
+        BorderPane borderPane = new BorderPane();
+
+        HBox horizontalBox = new HBox();
+        Button button = new Button("About");
+        button.setPrefSize(75,75);
+        Button saveButton = new Button("Save");
+        saveButton.setPrefSize(75,75);
+        Button loadButton = new Button("Load");
+        loadButton.setPrefSize(75,75);
+
+        horizontalBox.setSpacing(5);
+        versionBox(horizontalBox, button);
+        horizontalBox.getChildren().addAll(button,saveButton,loadButton);
+
+        VBox verticalBox = new VBox();
+        Button deleteButton = new Button("Delete");
+        deleteButton.setPrefSize(75,75);
+        Button createFolder = new Button("Create Folder");
+        createFolder.setPrefSize(75,75);
+        Button homeButton = new Button("Home");
+        homeButton.setPrefSize(75,75);
+        verticalBox.getChildren().addAll(deleteButton,createFolder,homeButton);
+
+        Pane pane = new Pane();
+        Text text1 = new Text("Welcome...");
+        pane.getChildren().add(text1);
+
+        // Places the layouts in the border pane
+        borderPane.setTop(horizontalBox);
+        borderPane.setRight(verticalBox);
+        borderPane.setCenter(text1);
+
+        return borderPane;
+    }
+    private GridPane loginScreen(Stage stage) {
+        GridPane gridPane = new GridPane();
+        Text username = new Text("Username/Email: ");
+        TextField inputUser = new TextField();
+
+        Text password = new Text("Password");
+        TextField inputPass = new TextField();
+
+        Button submit = new Button("Submit");
+        Button quit = new Button("Quit");
+
+        gridPane.setMinSize(200, 200);
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+
+        gridPane.setAlignment(Pos.CENTER);
+
+        gridPane.add(username, 0, 0);
+        gridPane.add(inputUser, 1, 0);
+        gridPane.add(password, 0, 1);
+        gridPane.add(inputPass, 1, 1);
+        gridPane.add(submit, 0, 2);
+        gridPane.add(quit, 1, 2);
+
+        accountInput(inputUser, inputPass, submit, stage);
+
+        return gridPane;
+    }
+
+    private void accountInput(TextField inputUser, TextField inputPass, Button submit, Stage stage) {
+       // if (inputUser.getCharacters().equals("5") && inputPass.getCharacters().equals("5")) {
+            Scene mainScene = new Scene(mainScreen(), 500, 500);
+            submit.setOnAction(e -> stage.setScene(mainScene));
+            stage.show();
+      //  }
+    }
+
+    private void versionBox(HBox hBox, Button button) {
         Dialog<String> dialog = new Dialog<>();
         dialog.setTitle("About");
         StringBuilder content = new StringBuilder();
@@ -58,10 +115,9 @@ public class GUIApplication extends Application {
         ButtonType exit = new ButtonType("Close", ButtonBar.ButtonData.CANCEL_CLOSE);
         dialog.getDialogPane().getButtonTypes().add(exit);
 
-        Button button = new Button("About");
         button.setOnAction(e -> dialog.showAndWait());
 
-        gridPane.add(button, 2, 0);
+//        hBox.getChildren().add(button);
     }
     public static void main(String[] args) {
 
--- src/main/Version.java
+++ src/main/Version.java
@@ -1,7 +1,7 @@
 package main;
 
 public class Version {
-    private static final double VERSION_NUMBER = 1.0;
+    private static final double VERSION_NUMBER = 1.1;
 
     public double getVersionNumber() {
         return this.VERSION_NUMBER;
