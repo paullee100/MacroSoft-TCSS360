554b146c141539b0146fc562e903d057b235bb7e
Adolf Hitler <not-available2009@hotmail.com>
2022-05-29 01:56:03 -0700

    added default constructor to ItemFile

--- src/main/data/ItemFile.java
+++ src/main/data/ItemFile.java
@@ -4,6 +4,11 @@ public class ItemFile {
     private String filePath;
     private String fileName;
 
+    public ItemFile() {
+        filePath = "";
+        fileName = "";
+    }
+
     public ItemFile(String filePath, String fileName) {
         this.filePath = filePath;
         this.fileName = fileName;
